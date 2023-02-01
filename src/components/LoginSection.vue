cd7ae72d0c597695c0e0473c6b8f312dbe6728b6
Misawai <69676474+Misawai@users.noreply.github.com>
2023-02-01 14:48:01 +0800

    fix(LoginSection.vue): fix typo in {{? :}}

--- src/components/LoginSection.vue
+++ src/components/LoginSection.vue
@@ -57,7 +57,7 @@ async function login() {
           <ui-spinner />
         </div>
         <div v-else class="card-title mb-2">
-          {{ session.isLogin ? t("components.loginSection.welcome") : t("components.loginSection.signin") }}{{ session.isLogin ? `${session.displayedName}` :  }}
+          {{ session.isLogin ? t("components.loginSection.welcome") : t("components.loginSection.signin") }} {{ session.isLogin ? `${session.displayedName}` : " " }}
         </div>
         <template v-if="session.isNotLogin">
           <div class="alert alert-error shadow-lg" v-if="loginForm.isError">
