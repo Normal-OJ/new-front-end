b3f13a6ab2d2dedf13ad69d1c343c8f02ca59362
TzuWei <ap9940506@gmail.com>
2023-07-16 17:12:26 +0800

    test: fix incorrect submit-code test case

--- tests/submit-code.spec.ts
+++ tests/submit-code.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from "@playwright/test";
 import { login } from "./utils/arranges";
+import { AC_510_CPP } from "./fixtures/submissions";
+
+test.beforeEach(async ({ page }) => {
+  await login(page);
+});
 
 test("submit code to A+B problem and get AC", async ({ page }) => {
-  await page.goto("/");
-  await login(page);
   await page.getByRole("link", { name: "Course" }).click();
   await page.getByRole("link", { name: "Test" }).click();
   await page.getByRole("link", { name: "Problems" }).click();
   await page.getByRole("link", { name: "510" }).click();
   await page.getByRole("link", { name: "Submit" }).click();
   await page.getByTestId("textarea").click();
-  await page.getByTestId("textarea").fill("asdfasdf");
+  await page.getByTestId("textarea").fill(AC_510_CPP);
   await page.getByRole("combobox").selectOption("1");
   await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page.getByText("Submission ID:")).toBeVisible();
   await expect(page).toHaveURL(/\/course\/Test\/submission\/[a-e\d]+/);
 
   const generalResult = await page.locator("table").nth(0);
