21a67fd115f0eeefa8b20274ec0d0de91b99de74
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-08-05 18:43:28 +0800

    feat: add mobile layout to index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { useTitle } from "@vueuse/core";
 
+import useInteractions from "@/composables/useInteractions";
+
+const { isDesktop } = useInteractions();
+
 useTitle("Home | Normal OJ");
 </script>
 
 <template>
-  <div class="mx-auto flex max-w-7xl gap-8 p-4">
+  <div
+    class="mx-auto flex max-w-7xl gap-8 overflow-y-scroll p-4"
+    :class="{
+      'flex-col-reverse gap-0 px-0': !isDesktop,
+    }"
+  >
     <system-announcements />
 
     <login-section />
