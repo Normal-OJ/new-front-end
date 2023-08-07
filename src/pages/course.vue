226593b84885c25e3e4b284f0d8a1edaba6a336f
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-08-07 23:21:05 +0800

    feat: update flex binding on size `lg` or smaller

--- src/pages/course.vue
+++ src/pages/course.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useInteractions from "@/composables/useInteractions";
+
+const { isDesktop } = useInteractions();
+</script>
 
 <template>
   <div>
     <course-top-bar />
-    <div class="flex">
+    <div
+      class="flex"
+      :class="{
+        'flex-col': !isDesktop,
+      }"
+    >
       <course-side-bar />
       <div class="flex-1">
         <router-view />
