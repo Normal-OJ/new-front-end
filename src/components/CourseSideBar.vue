5a285ede563f77289c14338f0a2f1f0b729e9467
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-08-07 23:17:10 +0800

    feat: use `tab` design on below `lg` size

--- src/components/CourseSideBar.vue
+++ src/components/CourseSideBar.vue
@@ -2,6 +2,10 @@
 import { useSession } from "@/stores/session";
 import { useI18n } from "vue-i18n";
 
+import useInteractions from "@/composables/useInteractions";
+
+const { isDesktop } = useInteractions();
+
 const { t } = useI18n();
 
 const session = useSession();
@@ -34,12 +38,25 @@ const navs = [
 </script>
 
 <template>
-  <ul class="menu menu-compact w-40 bg-base-100 p-2 lg:menu-normal">
-    <li
-      v-for="{ name, path } in navs"
-      :class="[{ 'border-l-4 border-blue-500': $route.path === `/course/${$route.params.name}${path}` }]"
-    >
-      <router-link :to="`/course/${$route.params.name}${path}`">{{ name }}</router-link>
-    </li>
-  </ul>
+  <template v-if="isDesktop">
+    <ul class="menu menu-compact w-40 bg-base-100 p-2 lg:menu-normal">
+      <li
+        v-for="{ name, path } in navs"
+        :class="[{ 'border-l-4 border-blue-500': $route.path === `/course/${$route.params.name}${path}` }]"
+      >
+        <router-link :to="`/course/${$route.params.name}${path}`">{{ name }}</router-link>
+      </li>
+    </ul>
+  </template>
+  <div v-else class="flex w-full justify-center p-2">
+    <div class="tabs items-center justify-center">
+      <div
+        v-for="{ name, path } in navs"
+        class="tab tab-bordered"
+        :class="[{ 'tab-active': $route.path === `/course/${$route.params.name}${path}` }]"
+      >
+        <router-link :to="`/course/${$route.params.name}${path}`">{{ name }}</router-link>
+      </div>
+    </div>
+  </div>
 </template>
