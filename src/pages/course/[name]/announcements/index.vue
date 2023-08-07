247dc38d1047e6286c453af6448501b124b90d54
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-08-07 23:22:09 +0800

    feat: update table width and layout for smaller devices

--- src/pages/course/[name]/announcements/index.vue
+++ src/pages/course/[name]/announcements/index.vue
@@ -6,6 +6,10 @@ import { formatTime } from "@/utils/formatTime";
 import { useSession } from "@/stores/session";
 import { useTitle } from "@vueuse/core";
 
+import useInteractions from "@/composables/useInteractions";
+
+const { isDesktop } = useInteractions();
+
 const session = useSession();
 const route = useRoute();
 
@@ -39,7 +43,7 @@ const {
             <skeleton-table :col="3" :row="5" />
           </template>
           <template #data>
-            <table class="table w-full">
+            <table v-if="isDesktop" class="table w-full">
               <thead>
                 <tr>
                   <th>{{ $t("course.ann.index.table.title") }}</th>
@@ -60,6 +64,43 @@ const {
                   </td>
                   <td>{{ creator.displayedName }}</td>
                   <td>{{ formatTime(createTime) }}</td>
+                  <td v-if="session.isAdmin">
+                    <div class="tooltip" data-tip="Edit">
+                      <router-link
+                        class="btn btn-ghost btn-sm btn-circle"
+                        :to="`/course/${$route.params.name}/announcements/${annId}/edit`"
+                      >
+                        <i-uil-edit class="lg:h-5 lg:w-5" />
+                      </router-link>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table v-else class="table w-full">
+              <thead>
+                <tr>
+                  <th>{{ $t("course.ann.index.table.title") }}</th>
+                  <th v-if="session.isAdmin"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="{ title, creator, createTime, annId } in announcements" :key="annId" class="hover">
+                  <td class="min-w-[10rem] max-w-[12rem] truncate">
+                    <router-link
+                      :to="`/course/${$route.params.name}/announcements/${annId}`"
+                      class="link-hover link"
+                    >
+                      {{ title }}
+                    </router-link>
+                    <br />
+                    <!-- we can't use flex if we want to truncate the text -->
+                    <span class="text-sm">
+                      {{ $t("course.ann.index.table.author") }}: {{ creator.displayedName }}</span
+                    >
+                    <br />
+                    <span class="text-sm">{{ formatTime(createTime) }}</span>
+                  </td>
                   <td v-if="session.isAdmin">
                     <div class="tooltip" data-tip="Edit">
                       <router-link
