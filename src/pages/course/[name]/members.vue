bc1012988ae9cf7933d3ca0004d114e10006d628
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2025-05-13 22:01:00 +0800

    Feat: add function to standardize uploaded csv in batch signup (#169)
    * feat: add function to standardize uploaded csv in batch signup
    
    * feat: add toggle for standardizeUsername
    
    * style: run formatter
    
    ---------
    
    Co-authored-by: uier <ap9940506@gmail.com>

--- src/pages/course/[name]/members.vue
+++ src/pages/course/[name]/members.vue
@@ -43,17 +43,38 @@ const rolesCanCreateCourse = [UserRole.Admin, UserRole.Teacher];
 
 const isOpen = ref(false);
 const newMembers = ref<File | null>();
+const shouldStandardizeUsername = ref(true);
 const newMembersCSVString = ref("");
 const forceUpdate = ref(false);
 const isProcessingSignup = ref(false);
 const errorMsg = ref("");
 const previewCSV = ref<{ headers?: string[]; body?: string[][] }>({});
+
+// no csv validation was handled
+const standardizeUsername = (csv: string): string => {
+  const rows = csv.split("\n");
+  const header = rows[0];
+  const body = rows.slice(1).map((r) => r.split(","));
+  const usernameFieldIndex = header.split(",").findIndex((v) => v === "username");
+  if (usernameFieldIndex < 0) {
+    return csv;
+  }
+
+  body.forEach((data) => {
+    data[usernameFieldIndex] = data[usernameFieldIndex]?.toUpperCase();
+  });
+
+  const bodyString = body.map((data) => data.join(","));
+  return [header, ...bodyString].join("\n");
+};
+
 watch(newMembers, () => {
   if (!newMembers.value) return;
   const reader = new FileReader();
   reader.onload = (evt) => {
     if (typeof evt.target?.result !== "string") return;
     newMembersCSVString.value = evt.target?.result;
+
     const rows = newMembersCSVString.value.split("\n");
     previewCSV.value.headers = rows[0].split(",");
     previewCSV.value.body = rows.slice(1).map((r) => r.split(","));
@@ -63,9 +84,13 @@ watch(newMembers, () => {
 async function submit() {
   if (!newMembersCSVString.value) return;
   isProcessingSignup.value = true;
+  const csv = shouldStandardizeUsername.value
+    ? standardizeUsername(newMembersCSVString.value)
+    : newMembersCSVString.value;
+
   try {
     await api.Auth.batchSignup({
-      newUsers: newMembersCSVString.value,
+      newUsers: csv,
       force: forceUpdate.value,
       course: route.params.name as string,
     });
@@ -159,6 +184,13 @@ async function submit() {
         </div>
 
         <div class="my-4" />
+
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <span class="label-text">{{ $t("course.members.standardizeUsername") }}</span>
+            <input v-model="shouldStandardizeUsername" type="checkbox" class="checkbox checkbox-primary" />
+          </label>
+        </div>
 
         <div class="alert alert-error shadow-lg" v-if="errorMsg">
           <div>
